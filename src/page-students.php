b5f191e28201567be08bd4d654bd2ae3264c3736
Dan Proudfoot <dan.proudfoot.iw@gmail.com>
2015-06-29 03:36:20 +0100

    Slope and ordering issue fixed on students. TODO: Remove pagination, fix SVGs, full eventbrite

--- src/page-students.php
+++ src/page-students.php
@@ -6,7 +6,8 @@ $context['page'] = Timber::get_post();
 $args = array(
 	'post_type' => 'student',
 	'post_status' => 'publish',
-	'posts_per_page' => '-1'
+	'posts_per_page' => '-1',
+	'orderby' => 'rand'
 );
 $context['students'] = Timber::get_posts($args);
 
